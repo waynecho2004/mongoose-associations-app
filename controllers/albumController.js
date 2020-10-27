8d26f9cab8da572d3f03144ab5e3281a18508f78
Wayne Cho <waynecho2004@gmail.com>
2020-10-26 21:07:14 -0400

    delete album and song

--- controllers/albumController.js
+++ controllers/albumController.js
@@ -75,7 +75,42 @@ router.put('/:albumId/songs/:songId', (req, res) => {
             res.redirect(`/albums/${foundAlbum.id}`);
         });
     });
-})  
+});
+
+// Delete a specic Song
+router.delete('/:albumId/songs/:songId', (req, res) => {
+    const albumId = req.params.albumId;
+    const songId = req.params.songId;
+    console.log('Delete song ' + songId);
+  
+    // find album in db by id and remove selected song
+    Album.findById(albumId, (error, foundAlbum) => {
+      // find song embbed in album
+      foundAlbum.songs.id(songId).remove();
+      // update song and complete with data from request body
+      foundAlbum.save((err, saveAlbum) => {
+        res.redirect(`/albums/${foundAlbum.id}`);
+      })
+    });
+  });
+
+// Delete a user
+router.delete('/:albumId', (req, res) => {
+    const albumId = req.params.albumId;
+    console.log('Delete album ' + albumId);
+  
+    // find user in db by id and remove selected tweet
+    Album.findById(albumId, (error, foundAlbum) => {
+      // find tweet embbed in user and remove them
+      foundAlbum.songs.forEach(song => {
+        foundAlbum.songs.id(song.id).remove();
+      })
+      Album.findByIdAndRemove(albumId, (err) => {
+        res.redirect('/albums');
+      })
+    });
+  
+  });
 
 // Index
 router.get('/', (req, res) => {
